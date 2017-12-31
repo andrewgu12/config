e44d8ff8bdf7bb35f32b28d872b05d7280a7f090
Andrew Gu <andrew@mingbogu.com>
2017-12-31 13:23:31 -0800

    redo custom emacs file

--- .emacs.d/elpa/ace-link-20170904.340/ace-link-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ace-link" "20170904.340" "Quickly follow links" '((avy "0.2.0")) :commit "34eefe3c94042a20abf4d340fdae71955a087926" :url "https://github.com/abo-abo/ace-link" :keywords '("convenience" "links"))
--- .emacs.d/elpa/persp-mode-20170908.648/persp-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "persp-mode" "20170908.648" "windows/buffers sets shared among frames + save/load." 'nil :commit "fd57484756b7a7e34895bf55238413005ab35959" :url "https://github.com/Bad-ptr/persp-mode.el" :keywords '("perspectives" "session" "workspace" "persistence" "windows" "buffers" "convenience"))
